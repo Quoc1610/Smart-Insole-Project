e5868f5a2c338ea18d64d0dec5bac0ce987e2b05
tuandao2013 <daotientuan2013@gmail.com>
2024-10-15 11:26:17 +0700

    add main menu UI

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Button btnImgTarget;
+    public Button btnPlain;
+    public GameObject goImgTarget;
+    public GameObject goPlain;
+
+
+    public void OnSetUp()
+    {
+        goImgTarget.SetActive(false);
+        goPlain.SetActive(false);
+    }
+    public void OnBtnImgTarget_Clicked()
+    {
+        goImgTarget.SetActive(true);
+        goPlain.SetActive(false);
+    }
+
+    public void OnBtnPlainTarget_Clicked()
+    {
+        goImgTarget.SetActive(false);
+        goPlain.SetActive(true);
+    }
+
     void Start()
     {
-        
+        OnSetUp();   
     }
 
     // Update is called once per frame
