314a23194ebc07a589a22e74b10553f2799553c4
Quoc1610 <tranquoc58668@gmail.com>
2024-10-16 15:38:20 +0700

    Add

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public UIMainMenu uiMainMenu;
+    public GameObject goJoystick;
     public static UIManager _instance { get; private set; }
+    private void Start() {
+        _instance = this;
+        goJoystick.SetActive(false);
+        
+    }
     public void OnLoadGameScene()
     {
         uiMainMenu.gameObject.SetActive(false);
